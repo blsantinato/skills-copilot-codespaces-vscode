d856ed62752dda6ec689a6f5eb7ecd546636d6fd
blsantinato <92471126+blsantinato@users.noreply.github.com>
2024-07-02 17:26:15 +0000

    Copilot Third commit

---
+++ comments.js
@@ -0,0 +1,41 @@
+// Create web server
+// 1. Create a new web server using express
+// 2. Create a new router
+// 3. Create a new route for POST /comments
+// 4. Read the data from the request body
+// 5. Save the data to the file comments.json
+// 6. Send a response to the client
+// 7. Listen on port 3000
+// 8. Create a new route for GET /comments
+// 9. Read the data from the file comments.json
+// 10. Send the data to the client
+// 11. Create a new route for GET /comments/:id
+// 12. Read the data from the file comments.json
+// 13. Find the comment with the given id
+// 14. Send the comment to the client
+// 15. Create a new route for PUT /comments/:id
+// 16. Read the data from the request body
+// 17. Read the data from the file comments.json
+// 18. Find the comment with the given id
+// 19. Update the comment with the new data
+// 20. Save the data to the file comments.json
+// 21. Send a response to the client
+// 22. Create a new route for DELETE /comments/:id
+// 23. Read the data from the file comments.json
+// 24. Find the comment with the given id
+// 25. Delete the comment from the file comments.json
+// 26. Send a response to the client
+// 27. Create a new route for GET /comments/:id/upvote
+// 28. Read the data from the file comments.json
+// 29. Find the comment with the given id
+// 30. Increment the upvotes of the comment
+// 31. Save the data to the file comments.json
+// 32. Send a response to the client
+// 33. Create a new route for GET /comments/:id/downvote
+// 34. Read the data from the file comments.json
+// 35. Find the comment with the given id
+// 36. Decrement the upvotes of the comment
+// 37. Save the data to the file comments.json
+// 38. Send a response to the client
+// 39. Create a new route for GET /comments/:id/comments
+// 40. Read
